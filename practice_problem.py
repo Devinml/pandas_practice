49c1426d34d36cd9776ec43163e11c641acfe324
Devinml <devinlink8@gmail.com>
2020-05-11 08:53:04 -0600

    added 3 problems

--- practice_problem.py
+++ practice_problem.py
@@ -17,5 +17,42 @@ exam_data = {'name':['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michae
 labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
 df = pd.DataFrame(data=exam_data,index= labels)
-print(df.head())
+#print(df.head())
+#print(df.info())
 
+'''
+Write a Pandas program to select the 'name' and 'score' columns from the following DataFrame.
+ Go to the editor
+Sample Python dictionary data and list labels:
+'''
+#print(df[['name','score']])
+
+'''
+ Write a Pandas program to select the specified columns and rows from a given data frame. 
+ Go to the editor
+Sample Python dictionary data and list labels:
+Select 'name' and 'score' columns in rows 1, 3, 5, 6 from the following data frame.
+'''
+#print(df)
+#print(df[['name','score']].iloc[[1,3,5,6]])
+
+'''
+Write a Pandas program to select the rows where the number of attempts in the examination is 
+greater than 2. Go to the editor
+Sample Python dictionary data and list labels:
+'''
+#print(df.loc[df['attempts']>2])
+
+'''
+Write a Pandas program to count the number of rows and columns of a DataFrame. Go to the editor
+Sample Python dictionary data and list labels:
+'''
+#num_of_rows = len(df)
+#num_of_columns = len(df.columns)
+#print(f'number of Rows = {num_of_rows}')
+#print(f'number of Columns = {num_of_columns}')
+
+'''
+Write a Pandas program to select the rows where the score is missing, i.e. is NaN. Go to the editor
+Sample Python dictionary data and list labels:
+'''
